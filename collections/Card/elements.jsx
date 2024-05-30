43bfeb6a9b08a6280d92622ca9ca32b10d8f4339
TeodoraNikolova <teodoraknikolova@abv.bg>
2024-05-30 15:52:54 +0300

    modified:   collections/Card/elements.jsx modified:   sections/Main/elements.jsx

--- collections/Card/elements.jsx
+++ collections/Card/elements.jsx
@@ -4,16 +4,32 @@ import styled from 'styled-components'
 
 export const StyledContainer = styled(({ ...props }) => <div {...props} />)`
 display: flex;
-max-width: 1920px;
-align-items: center;
-width: 100%;
-height: auto;
-border-radius: 6px;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 300px; 
+  height: 400px; 
+  border-radius: 6px;
+  overflow: hidden;
+  transition: transform 0.3s, box-shadow 0.3s;
 
-&:hover {
-  border: 2px solid #3F51B5; 
-  color: #3F51B5; 
-}
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  }
+
+  @media (max-width: 1100px) {
+    max-width: 90%;
+    height: auto;
+  }
+
+  @media (max-width: 600px) {
+    max-width: 100%;
+  }
+
+  @media (max-width: 350px) {
+    padding: 0.5rem;
+  }
 `;
 
 export const StyledDescription = styled(({ ...props }) => <div {...props} />)`
@@ -21,6 +37,17 @@ font-family: Poppins;
 font-size: 1.25rem;
 line-height: 1.875rem;
 font-weight: 300;
+text-align: center;
+
+@media (max-width: 600px) {
+  font-size: 1rem;
+  line-height: 1.5rem;
+}
+
+@media (max-width: 350px) {
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+}
 `;
 
 export const StyledInnerHeading = styled(({ ...props }) => <h1 {...props} />)`
@@ -29,24 +56,58 @@ font-size: 1.5rem;
 line-height: 1.5rem;
 font-weight: 700;
 text-decoration: underline;
+text-align: center;
+
+@media (max-width: 600px) {
+  font-size: 1.25rem;
+  line-height: 1.25rem;
+}
+
+@media (max-width: 350px) {
+  font-size: 1rem;
+  line-height: 1rem;
+}
 `;
 
 export const StyledTinyText = styled((props) => <p {...props} />)`
-  font-family: Poppins;
-  font-size: 1rem;
-  line-height: 1.5rem;
-  font-weight: 400;
-  color: black;
+font-family: Poppins;
+font-size: 1rem;
+line-height: 1.5rem;
+font-weight: 400;
+color: black;
+text-align: center;
+
+@media (max-width: 600px) {
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+}
+
+@media (max-width: 350px) {
+  font-size: 0.75rem;
+  line-height: 1rem;
+}
 `;
 
 export const StyledImageContainer  = styled(({ ...props }) => <div {...props} />)`
-  display: flex;
-  max-width: auto;
-  max-height: auto;
-  width: 70%;
-  height: 70%;
-  justify-content: center;
-  padding: 3rem;
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100%;
+height: auto;
+padding: 1rem;
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+@media (max-width: 600px) {
+  padding: 0.5rem;
+}
+
+@media (max-width: 350px) {
+  padding: 0.25rem;
+}
 `;
 
 
--- sections/Main/elements.jsx
+++ sections/Main/elements.jsx
@@ -7,11 +7,10 @@ import{
 
 
 export const StyledMainContainer = styled((props) => <div {...props} />)`
-	display: flex;
-	align-items: center;
-	flex-direction: column;
-	margin: 5rem auto;
-
+display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 5rem auto;
   background-image: url('/img/background.png');
   background-size: cover;
   background-position: center;
@@ -29,11 +28,11 @@ margin: 0;
 `;
 
 export const StyledImageContainer = styled(({ ...props }) => <div {...props} />)`
-  max-width: 32.03rem;
-  max-height: 65rem;
-  width: 100%;
-  height: 100%;
-  justify-content: center;
+max-width: 32.03rem;
+max-height: 65rem;
+width: 100%;
+height: 100%;
+justify-content: center;
 `;
 
 export const StyledDescriptionContainer  = styled((props) => <div {...props} />)`
@@ -57,42 +56,42 @@ width: 100%;
 `;
 
 export const StyledContainer = styled((props) => <div {...props} />)`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 100%;
-  height: auto;
-  margin: 0 auto;
-  gap: 1rem;
+display: flex;
+flex-direction: column;
+justify-content: center;
+width: 100%;
+height: auto;
+margin: 0 auto;
+gap: 1rem;
 
-  & > div:nth-child(1) {
-    width: 70%;
-    background-color: #F2F2F2;
-  }
+& > div:nth-child(1) {
+  width: 70%;
+  background-color: #F2F2F2;
+}
 
-  & > div:nth-child(2) {
-    width: 80%;
-    background-color: #E6E6E6;
-  }
+& > div:nth-child(2) {
+  width: 80%;
+  background-color: #E6E6E6;
+}
 
+& > div:nth-child(3) {
+  width: 90%;
+  background-color: #D9D9D9;
+}
+
+@media (max-width: 1100px) {
+  & > div:nth-child(1),
+  & > div:nth-child(2),
   & > div:nth-child(3) {
-    width: 90%;
-    background-color: #D9D9D9;
+    width: 100%;
   }
+}
 
-  @media (max-width: 1100px) {
-    & > div:nth-child(1),
-    & > div:nth-child(2),
-    & > div:nth-child(3) {
-      width: 100%;
-    }
-  }
+@media (max-width: 600px) {
+  gap: 0.5rem;
+}
 
-  @media (max-width: 600px) {
-    gap: 0.5rem;
-  }
-
-  @media (max-width: 350px) {
-    gap: 0.25rem;
-  }
+@media (max-width: 350px) {
+  gap: 0.25rem;
+}
 `;
